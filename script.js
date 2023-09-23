e5e85bb2518d378e2e588aae7739c88d3a908edd
Dundo <107737568+ganud@users.noreply.github.com>
2023-09-22 22:13:00 -0400

    Setup ui CSS

--- script.js
+++ script.js
@@ -63,26 +63,6 @@ function update_scores(result) {
         verdict.textContent = "You lost the match!";
     }
 }
-// Initialize player_wins and computer_wins variable
-// Add event listener to all buttons
-// Check if outcome win or loss
-    //Then update variables accordingly
-// Check if either side has 5 wins
-    //Then declare winner
-let player_wins = 0;
-let computer_wins = 0;
-let outcome = document.getElementById('outcome');
-let choices = document.querySelectorAll("button");
-let player_score = document.getElementById('playerscore');
-let computer_score = document.getElementById('computerscore')
-let verdict = document.querySelector('p');
 
-choices.forEach((choice) => {
-    choice.addEventListener("click", function () {
-        let userChoice = String(choice.textContent.toLowerCase());
-        result = playRound(userChoice, getComputerChoice());
-        outcome.textContent = result;
-        // Update score display
-        update_scores(result)
-    })
-} );
+// What if we turned RPS into fumos
+// Choices include cirno tewi and reisen lmao
